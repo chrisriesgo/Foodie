bfcc7aae8207b6d3d9a0ebb2a86d77551fcefd78
Ben Henderson <ben.henderson@fireflylogic.com>
2014-07-15 08:33:28 -0500

    Toggle fav

--- Foodie/View/WebsitePage.cs
+++ Foodie/View/WebsitePage.cs
@@ -1,14 +1,24 @@
 ﻿using System;
 using Xamarin.Forms;
 using Foodie.Model;
+using Foodie.Service;
 
 namespace Foodie
 {
 	public class WebsitePage: ContentPage
 	{
+		Restaurant _restaurant;
+
 		public WebsitePage (Restaurant restaurant)
 		{
+			_restaurant = restaurant;
+
 			Title = restaurant.Name;
+
+			ToolbarItems.Add (new ToolbarItem () {
+				Name = "fav",
+				Command = new Command( () => ToggleFavorite() )
+			});
 
 			Content = new WebView
 			{
@@ -18,6 +28,14 @@ namespace Foodie
 				},
 				VerticalOptions = LayoutOptions.FillAndExpand
 			};
+		}
+			
+		void ToggleFavorite ()
+		{
+			_restaurant.IsFavorite = !_restaurant.IsFavorite;
+
+			var service = DependencyService.Get<IRestaurantService> ();
+			service.SetFavoriteRestaurant (_restaurant.Id, _restaurant.IsFavorite);
 		}
 	}
 }
