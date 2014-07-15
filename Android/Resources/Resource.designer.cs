64d51d4b91e42dd709942be04a282d565bf7ed19
Macie Hummer <macie.hummer@fireflylogic.com>
2014-07-15 15:09:32 -0500

    added icons to the tab views

--- Android/Resources/Resource.designer.cs
+++ Android/Resources/Resource.designer.cs
@@ -45,7 +45,13 @@ namespace Foodie.Android
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			public const int fav = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int Icon = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int nearby = 2130837506;
 			
 			static Drawable()
 			{
